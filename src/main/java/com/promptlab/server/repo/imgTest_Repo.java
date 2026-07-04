6e7fcc64600ed4bc469c728f0968032034342d21
Amaan Sayyed <amaansayyed1@gmail.com>
2026-07-04 11:41:21 +0530

    folders managed

---
+++ src/main/java/com/promptlab/server/repo/imgTest_Repo.java
@@ -0,0 +1,9 @@
+package com.promptlab.server.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.promptlab.server.entity.Img;
+
+public interface imgTest_Repo extends JpaRepository<Img,Integer> {
+
+}
